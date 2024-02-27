0a6c7e2bae31fe1f3bfe808ad633b61b553792fd
wuha.csb <wuha.csb@alibaba-inc.com>
2024-02-27 14:47:54 +0800

    fix relative path issue of tpch/data_kit

--- benchmark/tpch/data_kit.sh
+++ benchmark/tpch/data_kit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 BASE="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if [[ ! -f ${BASE}/../util.sh ]]; then
+if [[ ! -f ${BASE}/../../util.sh ]]; then
     echo "Missing util.sh"
     exit 1
 fi
 
-source ${BASE}/../util.sh
+source ${BASE}/../../util.sh
 
 function usage()
 {
